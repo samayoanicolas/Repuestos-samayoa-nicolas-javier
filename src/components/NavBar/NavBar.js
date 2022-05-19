4850019430534b6b24ff32c153028e4793d9ffcd
samayoanicolas <javier.samayoa.nicolas@gmail.com>
2022-05-19 17:53:53 -0300

    Agregue carrito

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,22 +1,22 @@
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
-
 import Toolbar from '@mui/material/Toolbar';
-
 import Button from '@mui/material/Button';
+import '../NavBar/NavBar.css'
+import Cart from '../Cart/Cart';
 
 
 const NavBar = () =>{
     return( 
      <AppBar position="static">
         <Toolbar>
-        <ul>
-            <li><button>Inicio</button></li>
-            <li><button>Producto</button></li>
-            <li><button>Contacto</button></li>
-            
+        <ul id="listNav">
+            <li class="listButton" ><Button color="inherit" variant='text'>Inicio</Button></li>
+            <li class="listButton" ><Button color="inherit" variant='text'>Tienda</Button></li>
+            <li class="listButton" ><Button color="inherit" variant='text'>Contacto</Button></li>
+                 
         </ul>
-          <Button color="inherit">Login</Button>
+        <Cart/>
         </Toolbar>
       </AppBar>
     
