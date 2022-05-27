1bde05762084009cdcb81c03523fa8e8246de261
samayoanicolas <javier.samayoa.nicolas@gmail.com>
2022-05-26 21:06:41 -0300

    maps y promises

--- src/components/Card/CardList.js
+++ src/components/Card/CardList.js
@@ -1,37 +1,81 @@
 import * as React from 'react';
+import { useState, useEffect } from 'react';   
 import Item from './Card';
 import { Grid } from '@mui/material';
 import '../Card/CardList.css'
 
 
 const CardList=()=>{
+    const [products, setProducts]= useState([])
     const productos = [
-        {
-            imagen: 'imagen',
-            producto: 'producto',
-            precio: 1600,
+        {   
+            id:1,
+            imagen : '/img/nafta.jpg',
+            producto: 'Filtro de Nafta',
+            precio: 2300,
+            descripcion:'',
+            stock : 15
         
         },
+        {   
+            id:2,
+            imagen : '/img/aire.jpg',
+            producto: 'Filtro de Aire',
+            precio: 1600,
+            descripcion:'',
+            stock : 10
         
+        },
+        {   
+            id:3,
+            imagen : '/img/aceite.jpg',
+            producto: 'Filtro de aceite',
+            precio: 3600,
+            descripcion:'',
+            stock : 8
+        
+        },
     ]
-    return(
+
+    const getProducts = () =>{
+       return new Promise((resolve, reject)=>{
+           setTimeout(()=>{
+            resolve(productos)
+           },2000)
+       
+       })
+    }
+   useEffect(()=>{
+      getProducts()
+      .then((res)=>{
+             setProducts(res)
+
+         })
+      .catch((err)=>{
+             console.log("Fallo la llamada")
+    
+         })
+   },[])
+        return(
         <>
         <h2>Mas Vendidos</h2>
+        {console.log("Statte:", products)}
         <Grid container  alignItems="center" justifyContent="center">
-            <Grid item md={3}>
-                <Item imagen={"/img/nafta.jpg"} producto={"Filtro de nafta"} precio={2500} />
-            </Grid>
-            <Grid item md={3}>
-                <Item imagen={"/img/aire.jpg"} producto={"Filtro de aire"} precio={900} />
-            </Grid>
-            <Grid item md={3}>
-                <Item imagen={"/img/aceite.jpg"} producto={"Filtro de aceite"} precio={3500} />
-            </Grid>
-            
+        {
+          products.map((productos) =>{
+              return(
+                <Grid item md={3}>
+                     <Item imagen={productos.imagen} producto={productos.producto} precio={productos.precio} />
+                </Grid>
+              )
+              
+          })
+        }   
         </Grid>
         </>
     )
 }
 
  export default CardList
+
 
