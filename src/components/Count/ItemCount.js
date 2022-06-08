e9fb7f351ea40eb542b1033364eb03b4681579de
samayoanicolas <javier.samayoa.nicolas@gmail.com>
2022-06-07 21:12:37 -0300

    sincronizar counter - Eventos

--- src/components/Count/ItemCount.js
+++ src/components/Count/ItemCount.js
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import '../Count/ItemCount.css'
 
 
-const CountItem=()=>{
+const CountItem=({setShowButton})=>{
     const [count, setCount] = useState(0)
     const stock = 7;
     
@@ -17,13 +17,15 @@ const CountItem=()=>{
         setCount(count - 1)
     }
     return(
+      <>
       <div className='contador'>
         <Button variant="contained" color="error" onClick={menosUno} disabled= {count===0}>-</Button>
            <p className='numerador'>{count}</p>
         <Button variant="contained" color="primary" onClick={masUno} disabled= {count===stock}>+</Button>
 
       </div>
-
+      <Button color="success" onClick={() => setShowButton(true)} >Comprar</Button>
+     </>
     )
 }
 
--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -4,12 +4,21 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import '../Card/Card.css'
+import CardActions from '@mui/material/CardActions';
+import Button from '@mui/material/Button';
+import CountItem from '../Count/ItemCount';
+import { Link } from 'react-router-dom';
+
+import { useState } from 'react';
+
 
 const ItemDetail = ({data}) => {
+  const [showButton, setShowButton] = useState(false)
 
-   console.log ("esto es data:", data)
+
 
     return(
+      <>
       <div className='card'>      
       <Card sx={{ maxWidth: 345 }}>
       
@@ -26,9 +35,21 @@ const ItemDetail = ({data}) => {
           <Typography variant="body2" color="text.secondary">
           Cantidad disponibles: {data.stock}
           </Typography>
+           
+          {!  showButton ?
+          <CountItem
+            setShowButton={setShowButton}
+          />
+           :
+          <Button color="success" ><Link to={'/cart'}> Finalizar Compra </Link></Button> }
+          
+          
+        
+
         </CardContent>
       </Card>
       </div>
+      </>
     )
 }
 
