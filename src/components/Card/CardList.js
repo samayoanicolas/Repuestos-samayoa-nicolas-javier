11cf3bbd0f32c841256c79decc73495a5eb606a1
samayoanicolas <javier.samayoa.nicolas@gmail.com>
2022-06-16 18:54:58 -0300

    firebase

--- src/components/Card/CardList.js
+++ src/components/Card/CardList.js
@@ -4,29 +4,35 @@ import Item from './Card';
 import { Grid } from '@mui/material';
 import '../Card/CardList.css'
 import productos from '../../data/productsMocks.js';
-
+import { collection, doc, getDocs } from 'firebase/firestore';
+import db from '../../data/firebaseConfig';
 
 const CardList=()=>{
     const [products, setProducts]= useState([])   
-    const getProducts = () =>{
-       return new Promise((resolve, reject)=>{
-           setTimeout(()=>{
-            resolve(productos)
-           },2000)
-       
-       })
-    }
-   useEffect(()=>{
-      getProducts()
-      .then((res)=>{
-             setProducts(res)
+   
 
-         })
-      .catch((err)=>{
-             
-    
-         })
+
+
+   useEffect(()=>{
+      getProducts().then((productos)=>{
+        console.log("ppp", productos)
+        setProducts(productos)
+
+      })
+     
    },[])
+   const getProducts = async()=>{
+    const productSnapshot = await getDocs(collection(db,"productos"));
+    const productList= productSnapshot.docs.map((doc)=>{
+        let product=doc.data
+        product.id=doc.id
+
+      return product
+
+    })
+
+
+  }
         return(
         <>
         <h2>Mas Vendidos</h2>
